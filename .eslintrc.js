540e609d4202579829bf4475f9f2b82f46332762
linfl <1126101262@qq.com>
2020-10-29 14:09:30 +0800

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -82,7 +82,7 @@ module.exports = {
     'space-before-function-paren': [
       'warn',
       {
-        anonymous: 'never',
+        anonymous: 'always',
         named: 'never',
         asyncArrow: 'always',
       },
